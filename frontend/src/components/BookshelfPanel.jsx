01bf6c4b651feb59626ffc060f20b8d5c7fbb180
tarabogavelli <bogavellitara@gmail.com>
2026-04-16 18:48:45 -0700

    Fix container alignment between transcript and shelves

--- frontend/src/components/BookshelfPanel.jsx
+++ frontend/src/components/BookshelfPanel.jsx
@@ -8,18 +8,20 @@ export default function BookshelfPanel({ shelves }) {
   }
 
   return (
-    <div className="bookshelves">
+    <div className="bookshelves-panel">
       <h2 className="shelves-title">Shelves</h2>
-      {shelfNames.map((name) => (
-        <div key={name} className="shelf">
-          <h3 className="shelf-name">{name}</h3>
-          <div className="shelf-books">
-            {shelves[name].map((book, i) => (
-              <BookCard key={`${name}-${i}`} book={book} />
-            ))}
+      <div className="bookshelves">
+        {shelfNames.map((name) => (
+          <div key={name} className="shelf">
+            <h3 className="shelf-name">{name}</h3>
+            <div className="shelf-books">
+              {shelves[name].map((book, i) => (
+                <BookCard key={`${name}-${i}`} book={book} />
+              ))}
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 }
